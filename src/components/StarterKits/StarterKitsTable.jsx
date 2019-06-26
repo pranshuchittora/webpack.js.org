c2ec0d6ae53aba521d6f3e1992c184e2ca9aba5e
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-06-26 22:37:34 +0530

    chore(starter): improves starter-kit layout for content
    makes info inline hence improving UX

--- src/components/StarterKits/StarterKitsTable.jsx
+++ src/components/StarterKits/StarterKitsTable.jsx
@@ -3,26 +3,35 @@ import Kits from './_starter-kits.json';
 import Link from '../Link/Link';
 
 const StarterKitsTable = props => (
-    <div className="table-body">
-    { Kits.map((kit, i) => (
-      <div className="table-tr" key={ i }>
+  <div className="table-body">
+    {Kits.map((kit, i) => (
+      <div className="table-tr" key={i}>
         <div className="table-td">
-          <div className="table-td-title">Project Name</div>
-          <div className="table-td-content">
-            <Link to={ kit.githubUrl }>{ kit.githubRepoName }</Link>
+          <div className="table-td-title">
+            Project Name{': '}
+            <span className="table-td-content">
+              <Link to={kit.githubUrl}>{kit.githubRepoName}</Link>
+            </span>
           </div>
         </div>
         <div className="table-td">
-          <div className="table-td-title">Maintainer</div>
-          <div className="table-td-content">{ kit.githubUserName }</div>
+          <div className="table-td-title">
+            Maintainer{': '}
+            <span className="table-td-content">{kit.githubUserName}</span>
+          </div>
         </div>
         <div className="table-td">
-          <div className="table-td-title">Tags</div>
-          <div className="table-td-content">
-            { kit.tags.map((tag, i) => (
-              <span key={ i } className="starter-kits__tag">{ tag }</span>
-            )) }
+          <div className="table-td-title">
+            Tags{': '}
+            <span className="table-td-content">
+              {kit.tags.map((tag, i) => (
+                <span key={i} className="starter-kits__tag">
+                  {tag}
+                </span>
+              ))}
+            </span>
           </div>
+          <br />
         </div>
       </div>
     ))}

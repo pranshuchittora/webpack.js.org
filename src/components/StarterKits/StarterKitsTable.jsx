dd5fd1b60c2755f9aee8c5459671e670f5375319
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-06-27 00:16:06 +0530

    chore(starter): changes the layout to table for better SEO
    changes layout to table for batter UX

--- src/components/StarterKits/StarterKitsTable.jsx
+++ src/components/StarterKits/StarterKitsTable.jsx
@@ -2,40 +2,43 @@ import React from 'react';
 import Kits from './_starter-kits.json';
 import Link from '../Link/Link';
 
+import './StarterKitsTable.scss';
+
 const StarterKitsTable = props => (
-  <div className="table-body">
-    {Kits.map((kit, i) => (
-      <div className="table-tr" key={i}>
-        <div className="table-td">
-          <div className="table-td-title">
-            Project Name{': '}
-            <span className="table-td-content">
-              <Link to={kit.githubUrl}>{kit.githubRepoName}</Link>
-            </span>
+  <table className="starter_table-root">
+    <tb className="table-body">
+      {Kits.map((kit, i) => (
+        <tr className="table-tr" key={i}>
+          <div className="table-td">
+            <div className="table-td-title">
+              Project Name{': '}
+              <span className="table-td-content">
+                <Link to={kit.githubUrl}>{kit.githubRepoName}</Link>
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="table-td">
-          <div className="table-td-title">
-            Maintainer{': '}
-            <span className="table-td-content">{kit.githubUserName}</span>
+          <div className="table-td">
+            <div className="table-td-title">
+              Maintainer{': '}
+              <span className="table-td-content">{kit.githubUserName}</span>
+            </div>
           </div>
-        </div>
-        <div className="table-td">
-          <div className="table-td-title">
-            Tags{': '}
-            <span className="table-td-content">
-              {kit.tags.map((tag, i) => (
-                <span key={i} className="starter-kits__tag">
-                  {tag}
-                </span>
-              ))}
-            </span>
+          <div className="table-td">
+            <div className="table-td-title">
+              Tags{': '}
+              <span className="table-td-content">
+                {kit.tags.map((tag, i) => (
+                  <span key={i} className="starter-kits__tag">
+                    {tag}
+                  </span>
+                ))}
+              </span>
+            </div>
           </div>
-          <br />
-        </div>
-      </div>
-    ))}
-  </div>
+        </tr>
+      ))}
+    </tb>
+  </table>
 );
 
 export default StarterKitsTable;
